670973008b05f042b860de94dc946f31f9801787
MichaLL27 <lazarashvili2704@gmail.com>
2023-03-27 21:07:12 +0400

    finish register

--- frontend/src/app/components/partials/input-validation/input-validation.component.ts
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -10,6 +10,8 @@ import { AbstractControl } from '@angular/forms';
 const VALIDATORS_MESSAGES: any = {
   required: 'Should not be empty',
   email: 'Email is not valid',
+  minLength: 'Field should be at least {{requiredLength}} characters long',
+  notMatch: 'Password and Confirm does not match',
 };
 @Component({
   selector: 'input-validation',
