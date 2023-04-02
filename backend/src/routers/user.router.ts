c5a161c237211ef58cc16605b9423d446010b93a
MichaLL27 <lazarashvili2704@gmail.com>
2023-04-03 03:49:07 +0400

    fix back problems

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -66,7 +66,7 @@ const generateTokenResponse = (user: any) => {
       email: user.email,
       isAdmin: user.isAdmin,
     },
-    "SomeRandomText",
+    process.env.JWT_SECRET!,
     {
       expiresIn: "30d",
     }
